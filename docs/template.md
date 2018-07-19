37c2a781d5d2acd8806681dad7105599a6edf9a5
thondery <thondery@163.com>
2018-07-19 14:06:47 +0800

    add tempalte module ...

---
+++ docs/template.md
@@ -0,0 +1,29 @@
+# template - 关于模版
+
+- 模版采用 `nunjucks` 模块
+- 放置在项目根目录下 `views/` 中，以 `.njk` 为后缀
+
+## 编写模版
+
+具体语法参照 [官方文档](https://mozilla.github.io/nunjucks/templating.html)
+
+```html
+<html>
+  <head>
+    <title>{{ title }}</title>
+  </head>
+  <body>
+    .....
+  </body>
+</html>
+```
+
+## 渲染模版
+
+```js
+export const home = (req, res, next) => {
+  res.render('template', {
+    title: '标题名称'
+  })
+}
+```
--- server/index.js
+++ server/index.js
@@ -1,30 +1,66 @@
 import http from 'http'
+import path from 'path'
+import fs from 'fs-extra'
 import express from 'express'
+import nunjucks from 'nunjucks'
 import bodyParser from 'body-parser'
 import methodOverride from 'method-override'
 import compress from 'compression'
 import cors from 'cors'
+import errorhandler from 'errorhandler'
 import passport from 'passport'
 
 import { HOST, PORT } from './config'
+import Status from './config/status'
 import apiV1 from './routes/api_v1'
 import { startegy } from './middlewares/auth'
 import restful from './middlewares/restful'
 
 const app = express()
+const viewsDir  = path.resolve(process.cwd(), 'views')
 
+// Views
+app.set('views', viewsDir)
+app.set('view engine', 'njk')
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+})
+
+// Bodys
 app.use(bodyParser.json({ limit: '1mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
 app.use(methodOverride())
 app.use(compress())
 
+// Oauth
 passport.use(startegy)
 app.use(passport.initialize())
 
+// Middlewares
 app.use(restful)
 
+// Routes
 app.use('/api/v1', cors(), apiV1)
 
+// 404 Not Found
+app.use('*', (req, res) => {
+  console.error('status:404; url:', req.method, req.originalUrl)
+  return res.status(404).render('error', { statusCode: 404, ...Status[404] })
+})
+
+// 500 Error
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorhandler())
+}
+else {
+  app.use( (err, req, res, next) => {
+    console.error('server 500 error: ', err)
+    return res.status(500).render('error', { statusCode: 500, ...Status[500] })
+  })
+}
+
+// Starting ...
 if (!module.parent) {
   const server = http.createServer(app)
   server.listen(PORT, HOST, () => {
